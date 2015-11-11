77e6469e50af832a21d18d30f81538ef3e09dbeb
vilson <vilson.junior@ilegra.com>
2015-11-11 17:28:48 -0200

    Add gradle test

---
+++ src/test/java/GradleSeleniumTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by vilson on 11/11/15.
+ */
+public class GradleSeleniumTest {
+    private WebDriver driver;
+
+    @Test
+    public void testeGradleSelenium(){
+        driver = new ChromeDriver();
+        driver.get("http://google.com");
+        System.out.println("GRADLE SUCCESS");
+        driver.quit();
+    }
+}
